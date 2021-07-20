789077d992858c5d8ec706bc5b0495b7d2405426
Thiago Augusto Gonçalves Fernandes Santos Silva <wstagf@gmail.com>
2021-07-20 00:09:52 -0300

    criado rota para sub modulos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,10 @@
+import { PerfilComponent } from './place/perfil/perfil.component';
+import { ActivitysComponent } from './place/activitys/activitys.component';
+import { WalletComponent } from './place/wallet/wallet.component';
+import { ClubComponent } from './user/club/club.component';
+import { ControlComponent } from './user/control/control.component';
+import { ExperienceComponent } from './user/experience/experience.component';
+import { PortalAccessComponent } from './portal-access/portal-access.component';
 import { SubscribeComponent } from './subscribe/subscribe.component';
 import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
@@ -6,6 +13,42 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
    { path: 'home', component: HomeComponent },
    { path: 'facaparte', component: SubscribeComponent },
+   { path: 'acesso', component: PortalAccessComponent },
+   {
+    path: 'usuario',
+    children: [
+      {
+          path: 'experiencias',
+          component: ExperienceComponent
+      },
+      {
+        path: 'controle',
+        component: ControlComponent
+      },
+      {
+        path: 'clube',
+        component: ClubComponent
+      },
+    ]
+  },
+   {
+    path: 'estabelecimento',
+    children: [
+      {
+          path: 'perfil',
+          component: PerfilComponent
+      },
+      {
+        path: 'atividades',
+        component: ActivitysComponent
+      },
+      {
+        path: 'carteira',
+        component: WalletComponent
+      },
+    ]
+  },
+   { path: '**', component: HomeComponent },
 ];
 
 @NgModule({
