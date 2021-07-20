f5d8625abf3845166e7c9c7ac06539d500a03707
Thiago Augusto Gonçalves Fernandes Santos Silva <wstagf@gmail.com>
2021-07-19 22:23:51 -0300

    finalizado menu principal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,7 @@ import { NavbarComponent } from './navbar/navbar.component';
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -6,10 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-
+  public menuVisible = 1;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  portalAcess(param: string): void {
+    if ( param === 'user') {
+      this.menuVisible = 2;
+    } else {
+      if ( param === 'place') {
+        this.menuVisible = 3;
+      }
+    }
+  }
+
+  logout(): void {
+    this.menuVisible = 1;
   }
 
 }
