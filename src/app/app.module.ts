5f8b7738280a487e56fb9516a87bb7c8330dbe6f
Thiago Augusto Gonçalves Fernandes Santos Silva <wstagf@gmail.com>
2021-07-19 23:31:49 -0300

    Adicionado componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,13 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
 import { SubscribeComponent } from './subscribe/subscribe.component';
+import { PortalAccessComponent } from './portal-access/portal-access.component';
+import { ExperienceComponent } from './user/experience/experience.component';
+import { ControlComponent } from './user/control/control.component';
+import { ClubComponent } from './user/club/club.component';
+import { PerfilComponent } from './place/perfil/perfil.component';
+import { ActivitysComponent } from './place/activitys/activitys.component';
+import { WalletComponent } from './place/wallet/wallet.component';
 
 @NgModule({
   declarations: [
@@ -15,6 +22,13 @@ import { SubscribeComponent } from './subscribe/subscribe.component';
     NavbarComponent,
     HomeComponent,
     SubscribeComponent,
+    PortalAccessComponent,
+    ExperienceComponent,
+    ControlComponent,
+    ClubComponent,
+    PerfilComponent,
+    ActivitysComponent,
+    WalletComponent,
   ],
   imports: [
     BrowserModule,
