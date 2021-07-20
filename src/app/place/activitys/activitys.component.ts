988d452a4155d8ebbef9cf678a21dcf265220da9
Thiago Augusto Gonçalves Fernandes Santos Silva <wstagf@gmail.com>
2021-07-19 23:30:01 -0300

    Criado componente Place/Activitys

---
+++ src/app/place/activitys/activitys.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cpde2-activitys',
+  templateUrl: './activitys.component.html',
+  styleUrls: ['./activitys.component.css']
+})
+export class ActivitysComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
