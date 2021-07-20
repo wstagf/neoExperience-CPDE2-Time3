dea15426218d8c42d6a08c00cd95e734fc7fe8f4
Thiago Augusto Gonçalves Fernandes Santos Silva <wstagf@gmail.com>
2021-07-19 23:31:34 -0300

    Criado componente User/Experience

---
+++ src/app/user/experience/experience.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cpde2-experience',
+  templateUrl: './experience.component.html',
+  styleUrls: ['./experience.component.css']
+})
+export class ExperienceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
