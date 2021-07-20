5c9e5a69b904090173e3e06cd42c12ff3175236e
Thiago Augusto Gonçalves Fernandes Santos Silva <wstagf@gmail.com>
2021-07-19 23:37:45 -0300

    criado modulo e rotas para estabelecimentos

---
+++ src/app/place/place.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PlaceRoutingModule } from './place-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    PlaceRoutingModule
+  ]
+})
+export class PlaceModule { }
