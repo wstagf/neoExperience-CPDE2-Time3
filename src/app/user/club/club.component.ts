b2add845d126c5ea9475f6165775d1f165b62942
Thiago Augusto Gonçalves Fernandes Santos Silva <wstagf@gmail.com>
2021-07-19 23:31:05 -0300

    Criado componente User/Club

---
+++ src/app/user/club/club.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cpde2-club',
+  templateUrl: './club.component.html',
+  styleUrls: ['./club.component.css']
+})
+export class ClubComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
