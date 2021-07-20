45b03247c18fa2d1c510a0ea9fbeb27f1fb849b9
Thiago Augusto Gonçalves Fernandes Santos Silva <wstagf@gmail.com>
2021-07-19 23:30:33 -0300

    Criado componente Place/Wallet

---
+++ src/app/place/wallet/wallet.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cpde2-wallet',
+  templateUrl: './wallet.component.html',
+  styleUrls: ['./wallet.component.css']
+})
+export class WalletComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
